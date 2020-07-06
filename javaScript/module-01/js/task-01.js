d1062290da966d4c7395d1ca5b25d977b30b1cae
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-06 10:28:38 +0300

    add first task

--- javaScript/module-01/js/task-01.js
+++ javaScript/module-01/js/task-01.js
@@ -1,17 +1,11 @@
-// 'use strict';
-// const buttonRef = document.querySelector('.task1__btn');
-// buttonRef.addEventListener('click', () => {
-//   const name = 'Генератора силового поля';
-//   let price = 1000;
+'use strict';
+const buttonRef = document.querySelector('.task1__btn');
+buttonRef.addEventListener('click', () => {
+  const name = 'Генератора силового поля';
+  let price = 1000;
 
-//   console.log(`Выбран "${name}" цена за штуку ${price} кредитов.`);
+  console.log(`Выбран "${name}" цена за штуку ${price} кредитов.`);
 
-//   price += 1000;
-//   console.log(`Выбран "${name}" цена за штуку ${price} кредитов.`);
-// });
-
-// 'use strict';
-// const buttonRef = document.querySelector('.task1__btn');
-// buttonRef.addEventListener('click', () => {
-
-// });
+  price += 1000;
+  console.log(`Выбран "${name}" цена за штуку ${price} кредитов.`);
+});
