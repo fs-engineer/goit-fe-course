26e198fd706fb14bc3c45848dde34b280146b1e8
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-06 11:15:46 +0300

    add ternar an task 2

--- javaScript/module-01/js/task-02.js
+++ javaScript/module-01/js/task-02.js
@@ -4,16 +4,15 @@ const formRef = document.querySelector('.order__btn');
 const avaibleGoodsRef = document.querySelector('#numb__goods');
 const orderGoodsRef = document.querySelector('#ordered__goods');
 formRef.addEventListener('click', () => {
-  // const total = 100;
-  // const ordered = 50;
   event.preventDefault();
   const total = Number(avaibleGoodsRef.value);
   console.log(total);
   const ordered = Number(orderGoodsRef.value);
 
-  if (ordered > total) {
-    console.log('На складе не достаточно товаров, укажите меньшее кол-во!');
-  } else {
-    console.log('Заказ оформлен, с вами свяжеться менеджер');
-  }
+  const message =
+    ordered > total
+      ? 'На складе не достаточно товаров, укажите меньшее кол-во!'
+      : `Заказ оформлен, с вами свяжеться менеджер. Вы заказали ${ordered} товаров`;
+
+  alert(message);
 });
