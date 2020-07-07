6a74b94c998687eb072034bce899d4df566d1dc8
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-08 02:37:28 +0300

    fix hw6

--- javaScript/module-01/js/task-06.js
+++ javaScript/module-01/js/task-06.js
@@ -2,23 +2,20 @@
 
 const buttonRef = document.querySelector('.task6__btn');
 buttonRef.addEventListener('click', () => {
-  let input;
   let total = 0;
+  let input;
 
   do {
-    input = prompt('Введите любое число!');
+    input = prompt('Введите любое число');
 
-    if (input === null) {
-      alert(`Сумма всех чисел: ${total}`);
-      break;
-    }
-
-    input = Number(input);
-    const checkInputIsNan = isNaN(input);
-    if (checkInputIsNan === true) {
-      alert('Вы ввели не число, попробуйте еще раз');
-    } else {
+    if (input !== null) {
+      if (Number.isNaN(+input)) {
+        alert('Вы ввели не число, попробуйте еще раз');
+        continue;
+      }
+      input = Number(input);
       total += input;
+      alert(`Сумма всех чисел: ${total}`);
     }
-  } while (true);
+  } while (input !== null);
 });
