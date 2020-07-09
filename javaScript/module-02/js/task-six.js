9909cca1204d90261ea792aba8da55125697d02c
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-09 13:53:53 +0300

    add task6

---
+++ javaScript/module-02/js/task-six.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let input;
+const numbers = [];
+let total = 0;
+do {
+  input = prompt("Введите любое число!");
+
+  if (input !== null) {
+    input = Number(input);
+    if (Number.isNaN(input)) {
+      alert("Вы ввели не число, попробуйте еще раз");
+      continue;
+    }
+    numbers.push(input);
+  } else {
+    for (let number of numbers) {
+      total += number;
+    }
+    alert(`Вы ввели числа на общую сумму [${total}].`);
+    break;
+  }
+} while (input !== null);
