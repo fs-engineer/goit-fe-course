9eb6875d5ba3093bf936945f4b22a195b9c418ac
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-09 12:34:21 +0300

    add task4

--- javaScript/module-02/js/task-five.js
+++ javaScript/module-02/js/task-five.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const checkForSpam = function (message) {
+  const messageUpper = message.toUpperCase();
+  let spamCheck;
+
+  if (messageUpper.includes("SALE") || messageUpper.includes("SPAM")) {
+    spamCheck = true;
+  } else {
+    spamCheck = false;
+  }
+
+  return spamCheck;
+};
+
+console.log(checkForSpam("Latest technology news"));
+
+console.log(checkForSpam("JavaScript weekly newsletter"));
+
+console.log(checkForSpam("Get best sale offers now!"));
+
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
