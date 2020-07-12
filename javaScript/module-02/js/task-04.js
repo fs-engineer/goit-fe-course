794886c209c26fc5e03677aed725c0e9fa6dc450
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-12 10:49:43 +0300

    delete comments in task4

--- javaScript/module-02/js/task-04.js
+++ javaScript/module-02/js/task-04.js
@@ -7,21 +7,6 @@ const formatString = (string) => {
 
   return newString;
 };
-// const formatString = (string) => {
-//   const stringLegnth = string.length;
-//   let newString;
-
-//   if (stringLegnth > 40) {
-//     const stringArray = string.split("");
-//     stringArray.length = 40;
-//     stringArray.push("...");
-//     newString = stringArray.join("");
-//   }
-//   if (stringLegnth < 40) {
-//     newString = string;
-//   }
-//   return newString;
-// };
 
 buttonRef.addEventListener("click", () => {
   console.log(formatString("Curabitur ligula sapien, tincidunt non."));
