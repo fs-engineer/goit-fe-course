4e7f5072c9bce4535b91585e666d0741a2d25a27
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-14 16:19:50 +0300

    add task05

---
+++ javaScript/module-03/js/task-05.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  let nameOfProducts = [];
+  for (const product of arr) {
+    if (product[prop] !== undefined) {
+      nameOfProducts.push(product[prop]);
+    } else {
+      continue;
+    }
+  }
+  return nameOfProducts;
+};
+
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category")); // []
