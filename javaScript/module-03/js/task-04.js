53cc3663fb684a029fdb0abe3d36b018160bb60c
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-14 15:56:56 +0300

    add task04

---
+++ javaScript/module-03/js/task-04.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const countTotalSalary = function (employees) {
+  let totalSalary = 0;
+  for (const employee in employees) {
+    totalSalary += employees[employee];
+  }
+  return totalSalary;
+};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
