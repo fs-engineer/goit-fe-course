2a673ba9f9880a54d86d54e3d91775c195054997
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-08-01 19:47:07 +0300

    add module-05

---
+++ javaScript/module-05/js/task-01.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Account = function (account) {
+  this.account = account;
+};
+
+Account.prototype.getInfo = function () {
+  console.log(this.account);
+};
+
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
