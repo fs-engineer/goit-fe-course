5d0e8e0a9a3c61cc37011690a9292c739c11e193
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-08-01 22:22:47 +0300

    add task-05

---
+++ javaScript/module-05/js/task-04.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    this._value += str;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this._value = str + this._value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+
+console.log(builder.value);
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
