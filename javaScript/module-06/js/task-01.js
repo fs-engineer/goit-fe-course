f77d5f5ec1f0d4936aca676003d213158db472d0
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-08-03 17:52:44 +0300

    add task1 in 6 module

---
+++ javaScript/module-06/js/task-01.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import users from './users.js';
+const getUserNames = array => {
+  let userName = [];
+  users.forEach(user => {
+    userName.push(user.name);
+  });
+  return userName;
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
